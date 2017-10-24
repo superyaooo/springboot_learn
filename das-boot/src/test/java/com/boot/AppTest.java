ce6087014240043b2a6f786cd175f15f7c30e9ce
superyaooo <superyaooo@gmail.com>
2017-10-24 00:16:28 -0500

    add testing examples

--- das-boot/src/test/java/com/boot/AppTest.java
+++ das-boot/src/test/java/com/boot/AppTest.java
@@ -1,38 +1,18 @@
 package com.boot;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import com.boot.controller.HomeController;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+public class AppTest {
+	
+	@Test
+    public void testApp() {
+       HomeController hc = new HomeController();
+       String result = hc.home();
+       assertEquals(result, "Das Boot, reporting for duty!!!");
     }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	
+	
 }
