811a38f8f7b42092092bfcb78ef3472ec070324d
superyaooo <superyaooo@gmail.com>
2017-11-16 23:57:33 -0600

    remove all files that are in gitignore

--- RestAPI/src/main/java/com/springdemo/rest/model/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package com.springdemo.rest.model;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
--- das-boot/readme.md
+++
@@ -1,1 +0,0 @@
-Project built via Plurualsight course "Creating Your First Spring Boot Application".
