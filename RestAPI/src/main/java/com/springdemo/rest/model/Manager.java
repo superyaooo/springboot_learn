c221377071e3392de79a8927b7820ca57a140d80
superyaooo <superyaooo@gmail.com>
2018-02-07 12:28:57 -0600

    add manager model

---
+++ RestAPI/src/main/java/com/springdemo/rest/model/Manager.java
@@ -0,0 +1,38 @@
+package com.springdemo.rest.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString(exclude = "password")
+@Entity
+public class Manager {
+	
+	public static final PasswordEncoder PASSWORD_ENCODER = new BCryptPasswordEncoder();
+	
+	private @Id @GeneratedValue Long id;
+	private String name;
+	private @JsonIgnore String password;
+	private String[] roles;
+	
+	public void setPassword(String password) {
+		this.password = PASSWORD_ENCODER.encode(password);
+	}
+	
+	protected Manager() {}
+	
+	public Manager(String name, String password, String... roles) {
+		this.name = name;
+		this.setPassword(password);
+		this.roles = roles;
+	}
+}
