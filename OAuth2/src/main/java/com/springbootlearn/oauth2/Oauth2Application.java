3b04d57b24bea1930f0856be22f888bd3c9c96b7
superyaooo <superyaooo@gmail.com>
2018-03-01 12:11:12 -0600

    update gradle dependency

--- OAuth2/src/main/java/com/springbootlearn/oauth2/Oauth2Application.java
+++ OAuth2/src/main/java/com/springbootlearn/oauth2/Oauth2Application.java
@@ -2,8 +2,10 @@ package com.springbootlearn.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
 public class Oauth2Application {
 
 	public static void main(String[] args) {
