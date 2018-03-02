4a69a9bd0614516c64d5e3bb9633187cc80be4e1
superyaooo <superyaooo@gmail.com>
2018-03-02 11:59:15 -0600

    update react code in RestAPI

--- RestAPI/src/main/js/app.js
+++ RestAPI/src/main/js/app.js
@@ -313,34 +313,25 @@ class UpdateDialog extends React.Component {
 
 		var dialogId = "updateEmployee-" + this.props.employee.entity._links.self.href;
 
-		var isManagerCorrect = this.props.employee.entity.manager.name == this.props.loggedInManager;
-		if (isManagerCorrect == false) {
-			return (
-					<div>
-						<a>Not your Employee</a>
-					</div>
-					)
-		}
-		else {
-			return (
-					<div>
-						<a href={"#" + dialogId}>Update</a>
+		return (
+				<div>
+					<a href={"#" + dialogId}>Update</a>
 
-						<div id={dialogId} className="modalDialog">
-							<div>
-								<a href="#" title="Close" className="close">X</a>
+					<div id={dialogId} className="modalDialog">
+						<div>
+							<a href="#" title="Close" className="close">X</a>
 
-								<h2>Update an employee</h2>
+							<h2>Update an employee</h2>
 
-								<form>
-									{inputs}
-									<button onClick={this.handleSubmit}>Update</button>
-								</form>
-							</div>
+							<form>
+								{inputs}
+								<button onClick={this.handleSubmit}>Update</button>
+							</form>
 						</div>
 					</div>
-				)
-		}
+				</div>
+			)
+//		}
 		
 		
 	}
@@ -462,8 +453,7 @@ class Employee extends React.Component {
 				<td>
 					<UpdateDialog employee={this.props.employee}
 								  attributes={this.props.attributes}
-								  onUpdate={this.props.onUpdate}
-								  loggedInManager={this.props.loggedInManager}/>
+								  onUpdate={this.props.onUpdate}/>
 				</td>
 				<td>
 					<button onClick={this.handleDelete}>Delete</button>
