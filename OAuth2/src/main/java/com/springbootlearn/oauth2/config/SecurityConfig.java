ec76c45f8e3a6a9ad6efbe35cff2910a7cf82625
superyaooo <superyaooo@gmail.com>
2018-03-03 00:17:43 -0600

    add configuration annotation to config class - OAuth2

--- OAuth2/src/main/java/com/springbootlearn/oauth2/config/SecurityConfig.java
+++ OAuth2/src/main/java/com/springbootlearn/oauth2/config/SecurityConfig.java
@@ -1,9 +1,13 @@
 package com.springbootlearn.oauth2.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+@Configuration
+@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
